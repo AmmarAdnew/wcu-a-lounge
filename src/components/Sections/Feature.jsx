f8bdb5b6b2de1e4a2fb5cc13a6d8611f6f322dbe
ammaradnew <ammaradnew@gmail.com>
2025-01-22 10:14:19 +0000

    hosted and changed the feature grid to flex

--- src/components/Sections/Feature.jsx
+++ src/components/Sections/Feature.jsx
@@ -20,8 +20,8 @@ const Feature = () => {
   }, []);
 
   return (
-    <section className="bg-white rounded-t-2xl dark:bg-gray-900">
-      <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+    <section className="bg-white rounded-t-2xl dark:bg-gray-900 h-screen">
+      <div className="relative flex items-center justify-center h-full ml-12 mr-12 gap-6 overflow-hidden">
         <div className="mr-auto place-self-center lg:col-span-7">
           <h1 className="feature-title max-w-2xl mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
             Check Out Our{" "}
